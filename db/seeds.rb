1ebea51b040c3de64b1b21840ce9089e987ad66c
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-14 14:34:00 +0200

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying database..."
+Restaurant.destroy_all
+
+
+puts "Creating a restaurant..."
+restaurants_attributes = [
+  {
+    name:         'Lumiere',
+    address:      'Widdergasse 5, 8001 Zurich',
+    phone_number: '0442115665',
+    category:     'french'
+  },
+  {
+    name:         'Minine',
+    address:      'Zeltweg 12, 8032 Zurich',
+    phone_number: '0442608929',
+    category:     'japanese'
+  },
+  name:         'Achi',
+    address:      'Brauerstrasse 4, 8004 Zurich',
+    phone_number: '0442548885',
+    category:     'chinese'
+  },
+  {
+    name:         'Bindella',
+    address:      'In Gassen 6, 8001 Zurich',
+    phone_number: '0442212546',
+    category:     'italian'
+  },
+  {
+    name:         'Zeughauskeller',
+    address:      'Bahnhofstrasse 28, 8001 Zurich',
+    phone_number: '0442201515',
+    category:     'belgian'
+  }
+ ]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+
+
+puts "Destroying database..."
+Review.destroy_all
+
+
+puts "Creating a review..."
+review_attributes = [
+  {
+    restaurant_id:  2,
+    content:       'Unique food',
+    rating:         5
+  },
+]
+
+
+Review.create!(review_attributes)
+puts 'Finished!'
+
